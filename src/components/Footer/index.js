30a053cfff677d0f90a4c54c6fe39872ecd00d37
bernardo.urquijo <bernardo.urquijo@strategix4.com>
2022-09-28 13:02:55 -0700

    first-aproach

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Footer = () => {
     return (
-        <div>
-            Este es el footer
-        </div>
+        <Container>
+            <Row>
+                <hr />
+                <Col md={1}>Home</Col>
+                <Col md={2}>A cerca de nosotros</Col>
+                <Col md={1}>Contactanos</Col>
+                <Col md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>
+            </Row>
+        </Container>
     );
 }
 export default Footer;
