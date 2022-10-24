94b0b178e88f8215221059572a39aa1504d93ed1
bernardo.urquijo <bernardo.urquijo@strategix4.com>
2022-10-23 17:37:26 -0700

    signin

--- server/api/User.js
+++ server/api/User.js
@@ -37,7 +37,7 @@ router.post('/signup', (req, res) => {
                 })
             } else {
                 // Try to create new user
-            
+
                 //Password handling
                 const saltRounds = 10;
                 bcrypt.hash(password, saltRounds).then(hashedPassword => {
@@ -86,9 +86,62 @@ router.post('/signup', (req, res) => {
 
 
 
-//sign in
-router.post('/signup', (req, res) => {
+//Signin
+router.post('/signin', (req, res) => {
+    let { email, password } = req.body;
+    email = email.trim();
+    password = password.trim();
 
+    if (email == "" || password == "") {
+        res.json({
+            status: "FAILED",
+            message: "Empty credentials supplied"
+        })
+    } else {
+        //Checks if the user exists
+        User.find({ email })
+            .then(data => {
+                if (data.length) {
+                    //User exists
+                    const hashedPassword = data[0].password;
+                    bcrypt.compare(password, hashedPassword).then(result => {
+                        if (result) {
+                            //Password match
+                            res.json({
+                                status: "SUCCESS",
+                                message: "Signin successful",
+                                data: data
+                            })
+                        } else {
+                            res.json({
+                                status: "FAILED",
+                                message: "Invalid password entered"
+                            })
+                        }
+                    })
+                        .catch(err => {
+                            res.json({
+                                status: "FAILED",
+                                message: "An error occurred while comparing passwords"
+                            });
+
+                        });
+
+                } else {
+                    res.json({
+                        status: "FAILED",
+                        message: "Invalid credentials entered!"
+                    })
+                }
+            })
+            .catch(err => {
+                res.json({
+                    status: "FAILED",
+                    message: "An error occured while checking for existing user"
+                });
+            })
+    }
 })
+
 
 module.exports = router;
