eb301e38dc171aa40cf10246ddfe48ff853f61df
bernardo.urquijo <bernardo.urquijo@strategix4.com>
2022-11-21 14:28:25 -0700

    recent-comics-link

--- client/src/components/CardSection/index.js
+++ client/src/components/CardSection/index.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react';
 import { useSnackbar } from 'notistack';
 import axios from "axios"
 import '../../styles/cardsection.css'
+import { Link } from 'react-router-dom';
 
 const CardSection = () => {
 
@@ -31,18 +32,21 @@ const CardSection = () => {
             <Row xs={1} md={4} className="g-4">
                 {comics.map((val) => (
                     <Col>
-                        <Card className='homecard'>
-                            <Card.Img
-                                className='imagecard'
-                                variant="top"
-                                src={`${process.env.REACT_APP_API_URL}/comic/image/${val._id}`}
-                            // width="473"
-                            // height="160"
-                            />
-                            <Card.Body>
-                                <Card.Title className='cardtitle'>{val.name}</Card.Title>
-                            </Card.Body>
-                        </Card>
+                        <Link to={"/comic/" + val._id} style={{ color: 'inherit', textDecoration: 'none' }}>
+
+                            <Card className='homecard'>
+                                <Card.Img
+                                    className='imagecard'
+                                    variant="top"
+                                    src={`${process.env.REACT_APP_API_URL}/comic/image/${val._id}`}
+                                // width="473"
+                                // height="160"
+                                />
+                                <Card.Body>
+                                    <Card.Title className='cardtitle'>{val.name}</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </Link>
                     </Col>
                 ))}
             </Row>
